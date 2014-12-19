5c327244afaed1220d598fb585303ca5af714190
Yoshitaka Senna Goto <yoshi@opdsgn.com>
2014-12-18 19:33:22 -0800

    初期設定

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require("gulp");
+var compass = require('gulp-compass');
+var frontnote = require("gulp-frontnote");
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+var plumber = require("gulp-plumber");
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./htdocs/"
+        }
+    });
+});
+
+gulp.task('compass', function() {
+  gulp.src('sass/**/*scss')
+	.pipe(plumber())
+    .pipe(compass({
+      config_file: './config.rb',
+      css: 'css',
+      sass: 'sass'
+    }))
+    .pipe(frontnote({
+        css: 'htdocs/wp-content/themes/senna/css/style.css'
+    }))
+    .pipe(gulp.dest('htdocs/wp-content/themes/senna/css'))
+    .pipe(reload({stream:true}));
+});
+
+gulp.task("default", ['compass', 'browser-sync'], function() {
+    gulp.watch(["js/**/*.js","!js/min/**/*.js"],["js"]);
+    gulp.watch("sass/**/*.scss",["compass"]);
+});
+
